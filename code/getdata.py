bac7a1a5c661adb9476653d262ea562d40bd5214
WilhelmBuitrago <wibuitragog@unal.edu.co>
2024-03-24 16:05:15 -0500

    Upload getdata.py add: return data

--- code/getdata.py
+++ code/getdata.py
@@ -1,6 +1,8 @@
 import os
 from utils import download_from_drive, unzip
 import pandas as pd
+import numpy as np
+from typing import Union
 
 
 class GetDataset():
@@ -15,20 +17,67 @@ class GetDataset():
         self.__id = id
         self.__folder = os.path.join(os.path.dirname(__file__),
                                      'Dataset')
-        visualize_dataframe = args.get("VisualizateDataframe")
-        createDataframe = args.get("CreateDataframe", False)
+        self.visualize_dataframe = args.get("VisualizateDataframe")
+        self.createDataframe = args.get("CreateDataframe", False)
+        self.args = args
+
         if not GetDataset.already_unzipped:
             self.__set_env()
             GetDataset.already_unzipped = True
-        if createDataframe:
-            dff = self.ConcatDataset()
-            if visualize_dataframe:
-                if len(args["VisualizateDataframe"]) < 2 or len(args["VisualizateDataframe"]) > 2:
-                    raise ValueError(
-                        "El argumento 'VisualizateDataframe' toma unica y exactamente dos valores [True/False, 'head'/'tail']")
-                if args["VisualizateDataframe"][0]:
-                    self.Visualizate(
-                        df=dff, mode=args["VisualizateDataframe"][1], cant=10)
+
+    def Outputdata(self) -> tuple:
+        dff = self.ConcatDataset(self.createDataframe)
+
+        if self.visualize_dataframe:
+            if len(self.args["VisualizateDataframe"]) < 2 or len(self.args["VisualizateDataframe"]) > 2:
+                raise ValueError(
+                    "El argumento 'VisualizateDataframe' toma unica y exactamente dos valores [True/False, 'head'/'tail']")
+            if self.args["VisualizateDataframe"][0]:
+                self.Visualizate(
+                    df=dff, mode=self.args["VisualizateDataframe"][1], cant=10)
+
+        if isinstance(dff, list):
+            datas_arrays = []
+            targets_arrays = []
+
+            for dfs in dff:
+                N = self.__len__(dfs)
+                Predictors = 2
+                targets = 1
+
+                G_x1 = len(eval(dfs['x1'][0]))
+                G_x2 = len(eval(dfs['x2'][0]))
+                G = max(G_x1, G_x2)
+
+                data_array = np.zeros((N, Predictors, G))
+                target_array = np.zeros((N, targets))
+
+                for i in range(N):
+                    data_array[i, 0, :G] = eval(dfs['x1'][i])
+                    data_array[i, 1, :G] = eval(dfs['x2'][i])
+                    target_array[i, 0] = float(dfs['band_gap_mean'][i])
+
+                datas_arrays.append(data_array)
+                targets_arrays.append(target_array)
+            return datas_arrays, targets_arrays
+
+        elif isinstance(dff, pd.DataFrame):
+            N = self.__len__(dff)
+            Predictors = 2
+            targets = 1
+
+            G_x1 = len(eval(dff['x1'][0]))
+            G_x2 = len(eval(dff['x2'][0]))
+            G = max(G_x1, G_x2)
+
+            data_array = np.zeros((N, Predictors, G))
+            target_array = np.zeros((N, targets))
+
+            for i in range(N):
+                data_array[i, 0, :G] = eval(dff['x1'][i])
+                data_array[i, 1, :G] = eval(dff['x2'][i])
+                target_array[i, 0] = float(dff['band_gap_mean'][i])
+            return data_array, target_array
 
     def __set_env(self):
         destination_path_zip = os.path.join(self.__folder,
@@ -37,7 +86,7 @@ class GetDataset():
         download_from_drive(self.__id, destination_path_zip)
         unzip(destination_path_zip, self.__folder)
 
-    def ConcatDataset(self) -> pd.DataFrame:
+    def ConcatDataset(self, createDataframe: bool = False) -> Union[list, pd.DataFrame]:
         dfs = []
         for df_name in os.listdir(os.path.join(self.__folder, 'Bandgap')):
             if df_name == 'dataset_Paper1_Fluorite.csv':
@@ -50,27 +99,41 @@ class GetDataset():
             else:
                 dfs.append(pd.read_csv(os.path.join(
                     self.__folder, 'Bandgap', df_name)))
-        dff = pd.concat(dfs, ignore_index=True)
-        dff = dff.drop(dff.columns[0], axis=1)
-        dff.to_csv(os.path.join(
-            self.__folder, 'Bandgap', 'Dataset.csv'))
-
-        return dff
+        if createDataframe:
+            dff = pd.concat(dfs, ignore_index=True)
+            dff = dff.drop(dff.columns[0], axis=1)
+            dff.to_csv(os.path.join(
+                self.__folder, 'Bandgap', 'Dataset.csv'))
+            return dff
+        else:
+            return dfs
 
     def __len__(self, df):
         return len(df)
 
     def Visualizate(self,
-                    df: pd.DataFrame,
+                    df: Union[list, pd.DataFrame],
                     mode: str = "head",
                     cant: int = 5):
-        if mode == "head":
-            print(df.head(cant))
-        elif mode == "tail":
-            print(df.tail(cant))
-        else:
-            raise ValueError("Modo no valido. Seleccione 'head' o 'tail'.")
+        if isinstance(df, list):
+            for dfs in df:
+                if mode == "head":
+                    print(dfs.head(cant))
+                elif mode == "tail":
+                    print(dfs.tail(cant))
+                else:
+                    raise ValueError(
+                        "Modo no valido. Seleccione 'head' o 'tail'.")
+        elif isinstance(df, pd.DataFrame):
+            if mode == "head":
+                print(df.head(cant))
+            elif mode == "tail":
+                print(df.tail(cant))
+            else:
+                raise ValueError("Modo no valido. Seleccione 'head' o 'tail'.")
 
 
 if __name__ == '__main__':
-    GetDataset()
+    data, target = GetDataset().Outputdata()
+    print(data[1][-1][0])
+    print(target[1][-1])
